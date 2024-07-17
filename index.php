c912a974681fb217686ddee1e89b10249925e470
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-07-17 03:09:25 -0600

    prueba

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a mi sitio web</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Bienvenido a mi sitio web</h1>";
+        echo "<p>Esta es una página simple en PHP.</p>";
+    ?>
+</body>
+</html>
