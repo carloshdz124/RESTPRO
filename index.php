7b4fa7a8885a7a5416088798176bc03f2299568b
Alfredo Cruz <luis.cruz7672@alumnos.udg.mx>
2024-07-17 16:11:11 -0600

    prueba n10

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
-<?php 
+<?php
 include_once ("header.php");
 ?>
+
 <body>
     <div class="container mt-5">
         <?php
-            echo "<div class='jumbotron'>";
-            echo "<h1 class='display-4'>Bienvenido a mi Proyecto Modular</h1>";
-            echo "<p class='lead'>Esta es una prueba x2.</p>";
-            echo "<hr class='my-4'>";
-            echo "<p>Puse bootstrap XD.</p>";
-            echo "<h1 class='display-4'>ª</h1>";
-            echo "<h1 class='display-4'>ª</h1>";
-            echo "<h1 class='display-4'>ª</h1>";
-            echo "<h1 class='display-4'>ª</h1>";
-            ?>
-
-            <p>Esto es una modificacion de prueba</p>
-
-            <?php 
-            echo "<h1 class='display-4'>ª</h1>";
-            echo "<h1 class='display-4'>ª</h1>";
-            echo "<h1 class='display-4'>ª</h1>";
-            echo "<h1 class='display-4'>ª</h1>";
-            echo "<h1 class='display-4'>ª</h1>";
-            echo "<h3 class='display-4'>PRUEBAS</h3>";
-            echo "</div>";
+        echo "<div class='jumbotron'>";
+        echo "<h1 class='display-4'>Bienvenido a mi Proyecto Modular</h1>";
+        echo "<p class='lead'>Esta es una prueba x2.</p>";
+        echo "<hr class='my-4'>";
+        echo "<p>Puse bootstrap XD.</p>";
+        echo "<h1 class='display-4'>ª</h1>";
+        echo "<h1 class='display-4'>ª</h1>";
+        echo "<h1 class='display-4'>ª</h1>";
+        echo "<h1 class='display-4'>ª</h1>";
+        ?>
+        <p>
+            Esto es una modificacion de prueba
+        </p>
+        <?php
+        echo "<h1 class='display-4'>ª</h1>";
+        echo "<h1 class='display-4'>ª</h1>";
+        echo "<h1 class='display-4'>ª</h1>";
+        echo "<h1 class='display-4'>ª</h1>";
+        echo "<h1 class='display-4'>ª</h1>";
+        echo "<h3 class='display-4'>PRUEBAS</h3>";
+        echo "</div>";
         ?>
     </div>
     <!-- Bootstrap JS and dependencies -->
@@ -32,4 +33,5 @@ include_once ("header.php");
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
