c5c033ecf460b770e6f67f4b6d2cb20de5f42e43
Alfredo Cruz <luis.cruz7672@alumnos.udg.mx>
2024-07-17 16:40:22 -0600

    Se agrego footer

--- index.php
+++ index.php
@@ -34,10 +34,6 @@ include_once ("header.php");
         echo "</div>";
         ?>
     </div>
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+<?php
+include_once ("footer.php");
+?>
