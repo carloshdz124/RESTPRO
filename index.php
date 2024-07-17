76cb35e36df6a2e8f5effe9c8cf1bc3614dd127c
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-07-17 03:21:21 -0600

    más pruebas

--- index.php
+++ index.php
@@ -3,12 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenido a mi sitio web</title>
+    <title></title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <?php
-        echo "<h1>Bienvenido a mi sitio web</h1>";
-        echo "<p>Aqui haremos nuestro Proyecto Modular.</p>";
-    ?>
+    <div class="container mt-5">
+        <?php
+            echo "<div class='jumbotron'>";
+            echo "<h1 class='display-4'>Bienvenido a mi Proyecto Modular</h1>";
+            echo "<p class='lead'>Esta es una prueba.</p>";
+            echo "<hr class='my-4'>";
+            echo "<p>Puse bootstrap XD.</p>";
+            echo "<a class='btn btn-primary btn-lg' href='#' role='button'>ª</a>";
+            echo "</div>";
+        ?>
+    </div>
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
