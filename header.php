6c63c874c48d61504a331e8d05aeab35c2393b7e
Alfredo Cruz <luis.cruz7672@alumnos.udg.mx>
2024-07-18 16:39:49 -0600

    menu index a vistas

--- header.php
+++ header.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="tools/styles.css" rel="stylesheet" >
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </head>
+
+<body
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+include_once "header.php";
+?>
+
+<div class="container mt-5">
+
+    <div class="row">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <i class="bi bi-door-open icon-index"></i>
+                <div class="card-body">
+                    <h5 class="card-title">login</h5>
+                    <a href="login.php" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <i class="bi bi-bounding-box-circles icon-index"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Mesas</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <i class="bi bi-list-task icon-index"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Tareas pre-apertura</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <i class="bi bi-card-text icon-index"></i>
+                <div class="card-body">
+                    <h5 class="card-title">reportes</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <i class="bi bi-device-ssd icon-index"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Estaciones</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <i class="bi bi-people-fill icon-index"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Personal</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+
+<?php
+include_once "footer.php";
+?>
