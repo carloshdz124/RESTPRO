aafe402f2af78739ba3587a3c750ed5b8a51873a
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-07-18 12:56:34 -0600

    plantilla

--- login.php
+++ login.php
@@ -1,11 +1,16 @@
-<?php
-include_once ("header.php");
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INICIAR SESIÓN</title>
+</head>
 <body>
-    <h1>INDEX</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
 </body>
-
-<?php
-include_once ("footer.php");
-?>
+</html>
