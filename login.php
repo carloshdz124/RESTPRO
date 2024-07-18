75bbef03bda55c61ff4abf8d628773a368534e94
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-07-18 11:30:27 -0600

    reset

--- login.php
+++ login.php
@@ -3,37 +3,9 @@ include_once ("header.php");
 ?>
 
 <body>
-    <div class="container mt-5">
-        <?php
-        echo "<div class='jumbotron'>";
-        echo "<h1 class='display-4'>Bienvenido a mi Proyecto Modular</h1>";
-        echo "<p class='lead'>Esta es una prueba x2.</p>";
-        echo "<hr class='my-4'>";
-        echo "<p>Puse bootstrap XD.</p>";
-        echo "<h1 class='display-4'>ª</h1>";
-        ?>
-        
-        <p>PRUEBA<p>
-        <p>PRUEBA<p>
-        <p>Esto es una modificacion de prueba</p>
-        <p>FUR<p>
-        <p>FUR<p>
-        <p>FUR<p>
-        <p>FUR<p>
-        <p>FUR<p>
-        <p>FUR<p>
-        <p>puro pinche chivas<p>
-        <p>puro pinche chivas<p>
-        <p>puro pinche chivas<p>
-        <p>puro pinche chivas<p>
-        <p>Arriba las chivaaaaaaaaaaas</p>
+    <h1>INDEX</h1>
+</body>
 
-        <?php
-        echo "<h1 class='display-4'>ª</h1>";
-        echo "<h3 class='display-4'>PRUEBAS</h3>";
-        echo "</div>";
-        ?>
-    </div>
 <?php
 include_once ("footer.php");
 ?>
