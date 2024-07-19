6222a44bca6e82df2639bd7ae139ef4596dcb208
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-07-18 23:28:14 -0600

    se agrego el gif de login

--- login.php
+++ login.php
@@ -10,13 +10,27 @@ include_once"header.php";
                     <label for="email">Correo Electrónico</label>
                     <input type="email" class="form-control" id="email" placeholder="Ingresa tu correo electrónico">
                 </div>
+                <br>
                 <div class="form-group">
                     <label for="password">Contraseña</label>
                     <input type="password" class="form-control" id="password" placeholder="Ingresa tu contraseña">
                 </div>
-                <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+                <br>
+                <div class="d-flex justify-content-center">
+                    <button type="button" class="btn btn-primary btn-block d-block w-50">Ingresar</button>
+                </div>
+                <div class="loading">
+                    <img src="imagenes/login.gif" alt="Ingresando...">
+                </div>
             </form>
         </div>
     </div>
+    <script>
+        document.getElementById('loginForm').addEventListener('button', function(event) {
+            event.preventDefault(); // Evita que el formulario se envíe
+            document.querySelector('.login-box form').style.display = 'none'; // Oculta el formulario
+            document.querySelector('.loading').style.display = 'block'; // Muestra el GIF de carga
+        });
+    </script>
 
 <?php include_once"footer.php"; ?>
