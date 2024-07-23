be518f4ea1dda2724a6af161fdd72153893e1e02
Alfredo Cruz <luis.cruz7672@alumnos.udg.mx>
2024-07-22 20:48:55 -0600

    Se avanzo en la vista de Mesas, se crearon los modal, faltan 2 por rellenar

---
+++ assets/tools/styles/estilos_mesas.php
@@ -0,0 +1,14 @@
+<style>
+
+.btn-estilo {
+    height:60px;
+    width:150px; 
+    background-color: #e4c8ba;
+}
+
+
+.mapa {
+    display: none;
+}
+
+</style>
--- vistas/mesas.php
+++ vistas/mesas.php
@@ -1,18 +1,204 @@
 <?php
-$ubicacion= "../";
+$ubicacion = "../";
 $titulo = "MESAS";
-include ($ubicacion."includes/header.php");
+include ($ubicacion . "includes/header.php");
+include ($ubicacion . "assets/tools/styles/estilos_mesas.php");
+
 
 ?>
-
-<div class="container mt-5">
-
-    Content area...
-    
+<div class="container mt-3">
+    <h1 class="text-center">MESAS</h1><br>
+    <div class="row centrar">
+        <div class="col">
+            <button class="btn btn-estilo" data-bs-toggle="modal" data-bs-target="#modalAsignarMesa">
+                Registrar mesa
+            </button>
+        </div>
+        <div class="col">
+            <button class="btn btn-estilo " data-bs-toggle="modal" data-bs-target="#modalReservacion">
+                Registrar reservacion
+            </button>
+        </div>
+        <div class="col">
+            <button class="btn btn-estilo " data-bs-toggle="modal" data-bs-target="#modalReservacionHoy">
+                Reservaciones del dia
+            </button>
+        </div>
+        <div class="col">
+            <button class="btn btn-estilo" data-bs-toggle="modal" data-bs-target="#modalListaEspera">
+                Lista de espera
+            </button>
+        </div>
+    </div>
+    <br>
+    <div style="text-align: right;">
+        <button type="button" class="btn dropdown-toggle" style="background-color:#ce9477" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            mapas
+        </button>
+        <ul class="dropdown-menu" style="background-color:#ce9477">
+            <li><a onclick="seleccionaMapa('mapa1')" class="dropdown-item">mapa 1</a></li>
+            <li><a onclick="seleccionaMapa('mapa2')" class="dropdown-item">mapa 2</a></li>
+        </ul>
+    </div>
+    <div id="mapa1" class="mapa container visible" style="width: 100%; height: 45vh; background-color: grey;">
+        <p>mapa 1</p>
+    </div>
+    <div id="mapa2" class="mapa container" style="width: 100%; height: 45vh; background-color: red;">
+        <p>mapa 2</p>
+    </div>
 </div>
+
+
+
+<!-- Modal Registrar Mesa -->
+<div class="modal fade" id="modalAsignarMesa" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Registrar mesa</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="was-validated" action="#" method="POST">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input placeholder="A nombre de quien sera su mesa" type="text" class="form-control" id="nombre"
+                            name="nombre" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="number" class="form-label">Numero de adultos.</label>
+                        <input placeholder="Cantidad de adultos" type="number" class="form-control" id="email"
+                            name="tb_nadultos" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="number" class="form-label">Numero de niños.</label>
+                        <input placeholder="Cantidad de niños" type="number" class="form-control" id="email"
+                            name="tb_nniños" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="text" class="form-label">Area deseada</label>
+                        <select name="sg_area" class="form-select" aria-label="Default select example">
+                            <option value="salon">Salon</option>
+                            <option value="terraza">Terraza</option>
+                            <option value="infantil">Area infantil</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Registrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Reservacion -->
+<div class="modal fade" id="modalReservacion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Registrar Reservacion</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="was-validated" action="#" method="POST">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input placeholder="A nombre de quien sera su mesa" type="text" class="form-control" id="nombre"
+                            name="nombre" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="number" class="form-label">Numero de adultos.</label>
+                        <input placeholder="Cantidad de adultos" type="number" class="form-control" id="email"
+                            name="tb_nadultos" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="number" class="form-label">Numero de niños.</label>
+                        <input placeholder="Cantidad de niños" type="number" class="form-control" id="email"
+                            name="tb_nniños" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="text" class="form-label">Area deseada</label>
+                        <select name="sg_area" class="form-select" aria-label="Default select example">
+                            <option value="salon">Salon</option>
+                            <option value="terraza">Terraza</option>
+                            <option value="infantil">Area infantil</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="time" class="form-label">Hora (Disponible solo de 12pm a 10pm)</label>
+                        <input min="12:00:00" max="21:00:00" type="time" class="form-control" name="tb_hora" required>
+                    </div>
+                </div>
+            </form>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Registrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Lista de Espera -->
+<div class="modal fade" id="modalListaEspera" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Lista espera</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="was-validated" action="#" method="POST">
+                <div class="modal-body">
+
+                </div>
+            </form>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Registrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Reservacion Del dia -->
+<div class="modal fade" id="modalReservacionHoy" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Reservaciones del dia</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="was-validated" action="#" method="POST">
+                <div class="modal-body">
+
+                </div>
+            </form>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Registrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<script>
+    function seleccionaMapa(containerId) {
+        // Oculta todos los contenedores
+        document.querySelectorAll('.mapa').forEach(function (container) {
+            container.style.display = 'none';
+        });
+        // Muestra el contenedor seleccionado
+        document.getElementById(containerId).style.display = 'block';
+    }
+    showContainer('mapa1');
+</script>
 
 <?php
 
-include_once($ubicacion."includes/footer.php");
+include_once ($ubicacion . "includes/footer.php");
 
 ?>
