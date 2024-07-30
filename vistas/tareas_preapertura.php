a6d9c1769fd3f690fe73f64f56379205689cda93
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-07-30 04:24:05 -0600

    Se cambia la vista, se cambian los radios por checkbox, se añaden 3 nuevos botones, (bloquear, desbloquear y buscar).

--- vistas/tareas_preapertura.php
+++ vistas/tareas_preapertura.php
@@ -4,7 +4,12 @@ $titulo = "TAREAS PREAPERTURA";
 include ($ubicacion."includes/header.php");
 ?>
 <div class="container mt-5">
-    <h1 class="text-center"><?php echo $titulo; ?></h1><br><br>
+    <h1 class="text-center"><?php echo $titulo; ?></h1><br>
+    <div class="form-group border border-black bg-dark text-white p-3 rounded-4 btn-group btn-group-lg">
+        <button type="button" class="btn btn-outline-danger btn-lg rounded-4 mx-2"><i class="bi bi-lock-fill"></i></button>
+        <button type="button" class="btn btn-outline-success btn-lg rounded-4 mx-2"><i class="bi bi-unlock-fill"></i></button>
+        <button type="button" class="btn btn-outline-info btn-lg rounded-4 mx-2"><i class="bi bi-search"></i></button>
+    </div>
     <form method="POST" action="#">
         <div class="row">
             <!-- Primera Columna -->
@@ -14,161 +19,201 @@ include ($ubicacion."includes/header.php");
                     <div class="justify-content-center">
                         <ul class="list-group">
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero1" value="Mesero 1">
-                                    <label class="form-check-label" for="mesero1">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero1" value="Mesero 1">
+                                    </div>
+                                    <label class="form-control" for="mesero1">
                                         Mesero 1
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero2" value="Mesero 2">
-                                    <label class="form-check-label" for="mesero2">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero2" value="Mesero 2">
+                                    </div>
+                                    <label class="form-control" for="mesero2">
                                         Mesero 2
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero3" value="Mesero 3">
-                                    <label class="form-check-label" for="mesero3">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero3" value="Mesero 3">
+                                    </div>
+                                    <label class="form-control" for="mesero3">
                                         Mesero 3
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero4" value="Mesero 4">
-                                    <label class="form-check-label" for="mesero4">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero4" value="Mesero 4">
+                                    </div>
+                                    <label class="form-control" for="mesero4">
                                         Mesero 4
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero5" value="Mesero 5">
-                                    <label class="form-check-label" for="mesero5">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero5" value="Mesero 5">
+                                    </div>
+                                    <label class="form-control" for="mesero5">
                                         Mesero 5
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero6" value="Mesero 6">
-                                    <label class="form-check-label" for="mesero6">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero6" value="Mesero 6">
+                                    </div>
+                                    <label class="form-control" for="mesero6">
                                         Mesero 6
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero7" value="Mesero 7">
-                                    <label class="form-check-label" for="mesero7">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero7" value="Mesero 7">
+                                    </div>
+                                    <label class="form-control" for="mesero7">
                                         Mesero 7
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero8" value="Mesero 8">
-                                    <label class="form-check-label" for="mesero8">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero8" value="Mesero 8">
+                                    </div>
+                                    <label class="form-control" for="mesero8">
                                         Mesero 8
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero9" value="Mesero 9">
-                                    <label class="form-check-label" for="mesero9">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero9" value="Mesero 9">
+                                    </div>
+                                    <label class="form-control" for="mesero9">
                                         Mesero 9
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero10" value="Mesero 10">
-                                    <label class="form-check-label" for="mesero10">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero10" value="Mesero 10">
+                                    </div>
+                                    <label class="form-control" for="mesero10">
                                         Mesero 10
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero11" value="Mesero 11">
-                                    <label class="form-check-label" for="mesero11">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero11" value="Mesero 11">
+                                    </div>
+                                    <label class="form-control" for="mesero11">
                                         Mesero 11
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero12" value="Mesero 12">
-                                    <label class="form-check-label" for="mesero12">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero12" value="Mesero 12">
+                                    </div>
+                                    <label class="form-control" for="mesero12">
                                         Mesero 12
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero13" value="Mesero 13">
-                                    <label class="form-check-label" for="mesero13">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero13" value="Mesero 13">
+                                    </div>
+                                    <label class="form-control" for="mesero13">
                                         Mesero 13
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero14" value="Mesero 14">
-                                    <label class="form-check-label" for="mesero14">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero14" value="Mesero 14">
+                                    </div>
+                                    <label class="form-control" for="mesero14">
                                         Mesero 14
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero15" value="Mesero 15">
-                                    <label class="form-check-label" for="mesero15">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero15" value="Mesero 15">
+                                    </div>
+                                    <label class="form-control" for="mesero15">
                                         Mesero 15
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero16" value="Mesero 16">
-                                    <label class="form-check-label" for="mesero16">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero16" value="Mesero 16">
+                                    </div>
+                                    <label class="form-control" for="mesero16">
                                         Mesero 16
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero17" value="Mesero 17">
-                                    <label class="form-check-label" for="mesero17">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero17" value="Mesero 17">
+                                    </div>
+                                    <label class="form-control" for="mesero17">
                                         Mesero 17
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero18" value="Mesero 18">
-                                    <label class="form-check-label" for="mesero18">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero18" value="Mesero 18">
+                                    </div>
+                                    <label class="form-control" for="mesero18">
                                         Mesero 18
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero19" value="Mesero 19">
-                                    <label class="form-check-label" for="mesero19">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero19" value="Mesero 19">
+                                    </div>
+                                    <label class="form-control" for="mesero19">
                                         Mesero 19
                                     </label>
                                 </div>
                             </li>
                             <li class="list-group-item">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="mesero" id="mesero20" value="Mesero 20">
-                                    <label class="form-check-label" for="mesero20">
+                                <div class="input-group mb-0">
+                                    <div class="input-group-text bg-dark">
+                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero20" value="Mesero 20">
+                                    </div>
+                                    <label class="form-control" for="mesero20">
                                         Mesero 20
                                     </label>
                                 </div>
@@ -203,6 +248,23 @@ include ($ubicacion."includes/header.php");
         </div><br><br><br>
     </form>
 </div>
+
+<script>
+    // Selecciona todas las casillas de verificación
+    const checkboxes = document.querySelectorAll('.form-check-input[type="checkbox"]');
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            // Desmarcar todas las casillas de verificación excepto la seleccionada
+            checkboxes.forEach(box => {
+                if (box !== checkbox) {
+                    box.checked = false;
+                }
+            });
+        });
+    });
+</script>
+
 <?php
 
 include_once($ubicacion."includes/footer.php");
