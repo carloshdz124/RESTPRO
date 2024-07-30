b634ae27f625483e1f998fd696597044a3e5e206
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-07-30 06:48:20 -0600

    Se añaden modals a los botones (a falta de la implementación).

--- vistas/tareas_preapertura.php
+++ vistas/tareas_preapertura.php
@@ -6,9 +6,9 @@ include ($ubicacion."includes/header.php");
 <div class="container mt-5">
     <h1 class="text-center"><?php echo $titulo; ?></h1><br>
     <div class="form-group border border-black bg-dark text-white p-3 rounded-4 btn-group btn-group-lg">
-        <button type="button" class="btn btn-outline-danger btn-lg rounded-4 mx-2"><i class="bi bi-lock-fill"></i></button>
-        <button type="button" class="btn btn-outline-success btn-lg rounded-4 mx-2"><i class="bi bi-unlock-fill"></i></button>
-        <button type="button" class="btn btn-outline-info btn-lg rounded-4 mx-2"><i class="bi bi-search"></i></button>
+        <button data-bs-toggle="modal" data-bs-target="#modalBloquear" class="btn btn-outline-danger btn-lg rounded-4 mx-2"><i class="bi bi-lock-fill"></i></button>
+        <button data-bs-toggle="modal" data-bs-target="#modalDesbloquear" class="btn btn-outline-success btn-lg rounded-4 mx-2"><i class="bi bi-unlock-fill"></i></button>
+        <button data-bs-toggle="modal" data-bs-target="#modalBuscar" class="btn btn-outline-info btn-lg rounded-4 mx-2"><i class="bi bi-search"></i></button>
     </div>
     <form method="POST" action="#">
         <div class="row">
@@ -249,6 +249,7 @@ include ($ubicacion."includes/header.php");
     </form>
 </div>
 
+<!-- Script de los checkbox para solo selecionar uno a la vez -->
 <script>
     // Selecciona todas las casillas de verificación
     const checkboxes = document.querySelectorAll('.form-check-input[type="checkbox"]');
@@ -264,6 +265,115 @@ include ($ubicacion."includes/header.php");
         });
     });
 </script>
+
+<!-- Modal Bloquear Mesero -->
+<div class="modal fade" id="modalBloquear" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Bloquear Mesero</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="nombre" class="form-label"><strong>Tiempo Bloqueado</strong></label>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="rb_block" id="opcionHoy"
+                            onclick="toggleOptions()" checked>
+                        <label class="form-check-label" for="opcionHoy">Hoy</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="rb_block" id="opcionVarios"
+                            onclick="toggleOptions()">
+                        <label class="form-check-label" for="opcionVarios">Varios Días</label>
+                    </div>
+                </div>
+                <div id="cuantosDias" class="hidden">
+                    <div class="mb-3">
+                        <label for="extraOption1" class="form-label"><strong>Fecha de Inicio</strong></label>
+                        <input min="<?php echo isset($fecha)? $fecha : date('Y-m-d'); ?>" type="date" class="form-control" id="startDate"
+                            onchange="setMinEndDate()">
+                    </div>
+                    <div class="mb-3">
+                        <label for="endDate" class="form-label"><strong>Fecha de Fin</strong></label>
+                        <input type="date" class="form-control" id="endDate" name="endDate">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Bloquear</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Desbloquear Mesero -->
+<div class="modal fade" id="modalDesbloquear" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Desbloquear Mesero</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="nombre" class="form-label"><strong>Motivo Desbloqueo</strong></label>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="rb_block" id="opcionHoy"
+                            onclick="toggleOptions()" checked>
+                        <label class="form-check-label" for="opcionHoy">Fin Descanso</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="rb_block" id="opcionVarios"
+                            onclick="toggleOptions()">
+                        <label class="form-check-label" for="opcionVarios">Fin Vacaciones</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="rb_block" id="opcionVarios"
+                            onclick="toggleOptions()">
+                        <label class="form-check-label" for="opcionVarios">Fin Castigo</label>
+                    </div>
+                </div>
+                <div id="cuantosDias" class="hidden">
+                    <div class="mb-3">
+                        <label for="extraOption1" class="form-label"><strong>Fecha de Desbloqueo</strong></label>
+                        <input min="<?php echo isset($fecha)? $fecha : date('Y-m-d'); ?>" type="date" class="form-control" id="startDate"
+                            onchange="setMinEndDate()">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Desbloquear</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Buscar Mesero -->
+<div class="modal fade" id="modalBuscar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Buscar Mesero</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="nombre" class="form-label"><strong>Ingrese el Nombre del Mesero</strong></label>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Mesero" aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Buscar</button>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 
