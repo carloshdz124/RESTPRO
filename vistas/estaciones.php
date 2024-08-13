dd88eea8a19c7f7d9b6b356d62774be4a6d0e5bf
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-08-13 08:47:16 -0600

    Se corrigue el boton de "Cambiar Estación", al igual que el CSS de "mapa" y "mesa" para que este sea más concorde a las especificaciones del restaurante.

--- vistas/estaciones.php
+++ vistas/estaciones.php
@@ -3,13 +3,32 @@ $ubicacion = "../";
 $titulo = "Estaciones";
 include ($ubicacion . "includes/header.php");
 ?>
+<!-- Se realiza una consulta para revisar si existen areas -->
+<?php
+$sql = "SELECT * FROM area";
+$result = $pdo->query($sql);
+if ($result->rowCount() > 0) {
+    $resultAreas = $result->fetchAll(PDO::FETCH_OBJ);
+} else {
+    $resultAreas = array();
+}
+?>
 <link href="assets/tools/styles.css" rel="stylesheet">
 <div class="container mt-5">
     <h1 class="text-center"><?php echo $titulo; ?></h1><br>
     <div class="form-group border border-black bg-dark text-white p-3 rounded-4">
-        <h2 class="text-center" for="columna1">Mapa Salón</h2>
-        <div class="btn-container">
-            <button class="btn btn-outline-info btn-lg rounded-4">Cambiar</button><br><br>
+        <h2 class="text-center" for="columna1">Mesas</h2>
+        <div class="text-end">
+            <div class="dropdown">
+                <button class="btn btn-outline-info btn-lg rounded-4 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Cambiar Estación
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-dark">
+                    <?php foreach ($resultAreas as $area) { ?>
+                        <li><a onclick="seleccionaMapa('<?php echo $area->id; ?>')" class="dropdown-item"><?php echo $area->nombre ?></a></li>
+                    <?php } ?>
+                </ul><br><br>
+            </div>
         </div>
         <div class="mapa">
             <div class="mesa">Mesa 1 - Z1</div>
@@ -42,8 +61,6 @@ include ($ubicacion . "includes/header.php");
             <div class="mesa">Mesa 28 - Z3</div>
             <div class="mesa">Mesa 29 - Z3</div>
             <div class="mesa">Mesa 30 - Z3</div>
-            <div class="mesa">Mesa 31 - Z4</div>
-            <div class="mesa">Mesa "N" - Z"N"</div>
         </div>
     </div>
 </div>
