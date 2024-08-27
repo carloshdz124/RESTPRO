fe5a9b725f25f78dff4b30368b411a9893417da5
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-08-27 02:54:37 -0600

    Se agrego la lista de meseros desde la base de datos.

--- vistas/tareas_preapertura.php
+++ vistas/tareas_preapertura.php
@@ -2,6 +2,14 @@
 $ubicacion = "../";
 $titulo = "TAREAS PREAPERTURA";
 include ($ubicacion."includes/header.php");
+include ($ubicacion."config/conexion.php");
+
+$result = $pdo->query("SELECT * FROM personal");
+if ($result->rowCount() > 0) {
+    $resultMeseros = $result->fetchAll(PDO::FETCH_OBJ);
+} else {
+    $resultMeseros = array();
+}
 ?>
 <div class="container mt-5">
     <h1 class="text-center"><?php echo $titulo; ?></h1><br>
@@ -18,206 +26,21 @@ include ($ubicacion."includes/header.php");
                     <h2 class="text-center" for="columna1">Meseros</h2><br>
                     <div class="justify-content-center">
                         <ul class="list-group">
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero1" value="Mesero 1">
+                            <?php foreach ($resultMeseros as $mesero): ?>
+                                <li class="list-group-item">
+                                    <div class="input-group mb-0">
+                                        <div class="input-group-text bg-dark">
+                                            <input class="form-check-input mt-0" type="checkbox" name="meseros[]" id="mesero<?php echo $mesero->id; ?>" value="<?php echo $mesero->id; ?>">
+                                        </div>
+                                        <span class="form-control text-primary-emphasis bg-primary-subtle border border-primary-subtle d-flex justify-content-between align-items-center" for="mesero<?php echo $mesero->id; ?>">
+                                            <?php echo htmlspecialchars($mesero->nombre . " " . $mesero->apellido); ?>
+                                        </span>
+                                        <button data-bs-toggle="modal" data-bs-target="#modalEditar" class="btn btn-outline-warning btn-lg rounded-4 mx-2" title="DETALLES">
+                                            <i class="bi bi-eye"></i>
+                                        </button>
                                     </div>
-                                    <label class="form-control" for="mesero1">
-                                        Mesero 1
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero2" value="Mesero 2">
-                                    </div>
-                                    <label class="form-control" for="mesero2">
-                                        Mesero 2
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero3" value="Mesero 3">
-                                    </div>
-                                    <label class="form-control" for="mesero3">
-                                        Mesero 3
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero4" value="Mesero 4">
-                                    </div>
-                                    <label class="form-control" for="mesero4">
-                                        Mesero 4
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero5" value="Mesero 5">
-                                    </div>
-                                    <label class="form-control" for="mesero5">
-                                        Mesero 5
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero6" value="Mesero 6">
-                                    </div>
-                                    <label class="form-control" for="mesero6">
-                                        Mesero 6
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero7" value="Mesero 7">
-                                    </div>
-                                    <label class="form-control" for="mesero7">
-                                        Mesero 7
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero8" value="Mesero 8">
-                                    </div>
-                                    <label class="form-control" for="mesero8">
-                                        Mesero 8
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero9" value="Mesero 9">
-                                    </div>
-                                    <label class="form-control" for="mesero9">
-                                        Mesero 9
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero10" value="Mesero 10">
-                                    </div>
-                                    <label class="form-control" for="mesero10">
-                                        Mesero 10
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero11" value="Mesero 11">
-                                    </div>
-                                    <label class="form-control" for="mesero11">
-                                        Mesero 11
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero12" value="Mesero 12">
-                                    </div>
-                                    <label class="form-control" for="mesero12">
-                                        Mesero 12
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero13" value="Mesero 13">
-                                    </div>
-                                    <label class="form-control" for="mesero13">
-                                        Mesero 13
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero14" value="Mesero 14">
-                                    </div>
-                                    <label class="form-control" for="mesero14">
-                                        Mesero 14
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero15" value="Mesero 15">
-                                    </div>
-                                    <label class="form-control" for="mesero15">
-                                        Mesero 15
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero16" value="Mesero 16">
-                                    </div>
-                                    <label class="form-control" for="mesero16">
-                                        Mesero 16
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero17" value="Mesero 17">
-                                    </div>
-                                    <label class="form-control" for="mesero17">
-                                        Mesero 17
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero18" value="Mesero 18">
-                                    </div>
-                                    <label class="form-control" for="mesero18">
-                                        Mesero 18
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero19" value="Mesero 19">
-                                    </div>
-                                    <label class="form-control" for="mesero19">
-                                        Mesero 19
-                                    </label>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="input-group mb-0">
-                                    <div class="input-group-text bg-dark">
-                                        <input class="form-check-input mt-0" type="checkbox" name="mesero" id="mesero20" value="Mesero 20">
-                                    </div>
-                                    <label class="form-control" for="mesero20">
-                                        Mesero 20
-                                    </label>
-                                </div>
-                            </li>
+                                </li>
+                            <?php endforeach; ?>
                         </ul>
                     </div>
                 </div>
