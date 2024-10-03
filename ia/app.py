26fb1139b768240fc00a2bfaf75ebb14975d6e63
Alfredo Cruz <luis.cruz7672@alumnos.udg.mx>
2024-10-03 02:44:27 -0600

    api-ia

--- ia/app.py
+++ ia/app.py
@@ -1,7 +1,38 @@
 # python/app.py
 from flask import Flask, jsonify, request
+import mysql.connector
+import random
 
 app = Flask(__name__)
+
+# Configuración de la conexión a la base de datos
+db_config = {
+    'host': 'db',  # El nombre del servicio de Docker
+    'user': 'root',
+    'password': 'root',
+    'database': 'db_restpro'
+}
+
+@app.route('/api/consulta', methods=['GET'])
+def get_productos():
+    try:
+        # Conectar a la base de datos
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM areas")
+        rows = cursor.fetchall()
+
+        # Convertir los datos en un diccionario
+        data = []
+        for row in rows:
+            data.append({'id': row[0], 'nombre': row[1], 'descripcion': row[2]})
+
+        return jsonify(data)
+    except mysql.connector.Error as err:
+        return jsonify({"error": str(err)}), 500
+    finally:
+        cursor.close()
+        conn.close()
 
 @app.route('/api/funcion', methods=['GET'])
 def funcion():
@@ -25,13 +56,36 @@ def resta():
 
 @app.route('/api/ia', methods=['POST'])
 def ia():
-    data = request.json  # Obtener los datos JSON enviados
-    dia = data.get('dia', 0)
-    personas = data.get('personas', 0)
-    hora = data.get('hora', 0)
-    minutos = data.get('minutos', 0)
-    resultado = dia * personas - (hora*minutos)
-    return jsonify({"resultado": resultado})
+    try:
+        data = request.json  # Obtener los datos JSON enviados
+        dia = data.get('dia', 0)
+        personas = data.get('personas', 0)
+        hora = data.get('hora', 0)
+        minutos = data.get('minutos', 0)
+        
+        if personas == 1:
+            personas += 1
+        elif (personas % 2 != 0):
+            personas -= 1
+        
+        # Conectar a la base de datos
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM ia WHERE personas = {personas} AND hora = {hora} AND minutos = {minutos}")
+        rows = cursor.fetchall()
+        
+        
+        # Convertir los datos en un diccionario
+        data = []
+        for row in rows:
+            data.append({'salida': row[4]})
+        data = random.choice(data)
+        return jsonify({"resultado": data})
+    except mysql.connector.Error as err:
+        return jsonify({"error": str(err)}), 500
+    finally:
+        cursor.close()
+        conn.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
