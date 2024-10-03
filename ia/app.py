227d8e3e1f340725a7d724a4d1a5e5987674e4e6
Alfredo Cruz <luis.cruz7672@alumnos.udg.mx>
2024-10-03 01:05:01 -0600

    se crearon nuvas apis

--- ia/app.py
+++ ia/app.py
@@ -5,15 +5,33 @@ app = Flask(__name__)
 
 @app.route('/api/funcion', methods=['GET'])
 def funcion():
-    return jsonify({"mensaje": "Hola desde Python y una apiiii"})
+    return jsonify({"mensaje": "Hola desde Python y una aaaaaapiiii tttttttttttttt"})
 
 @app.route('/api/suma', methods=['POST'])
 def suma():
-    data = request.json  # Obtener los datos JSON enviados
-    num1 = data.get('num1', 0)  # Obtener el primer número
-    num2 = data.get('num2', 0)  # Obtener el segundo número
-    resultado = num1 + num2  # Realizar la suma
+    data = request.json
+    num1 = data.get('num1', 0)
+    num2 = data.get('num2', 0)
+    resultado = num1 + num2
+    return jsonify({"resultado": resultado})
+
+@app.route('/api/resta', methods=['POST'])
+def resta():
+    data = request.json
+    num1 = data.get('num1', 0)
+    num2 = data.get('num2', 0) 
+    resultado = num1 - num2
     return jsonify({"resultado": resultado})  # Devolver el resultado
+
+@app.route('/api/ia', methods=['POST'])
+def ia():
+    data = request.json  # Obtener los datos JSON enviados
+    dia = data.get('dia', 0)
+    personas = data.get('personas', 0)
+    hora = data.get('hora', 0)
+    minutos = data.get('minutos', 0)
+    resultado = dia * personas - (hora*minutos)
+    return jsonify({"resultado": resultado})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
